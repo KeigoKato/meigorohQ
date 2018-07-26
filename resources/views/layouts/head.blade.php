3e3ea95f1b094861dca79b34c5d83345ff707e79
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-26 23:07:25 +0900

    [fix] cssファイルを適用
    bladeのasset関数を使う

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <title>@yield('title') | meigorohQ</title>
     <!-- リセットcss -->
-    <!-- <link rel="stylesheet" href="/freemarket/share/reset.css", type="text/css"> -->
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}", type="text/css">
     <!-- Googleフォント -->
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
     <!--BootstrapのCSS -->
@@ -11,5 +11,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <!-- 共通のcss -->
-    <!-- <link rel="stylesheet" href="share/style.css", type="text/css"> -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}", type="text/css">
 </head>
