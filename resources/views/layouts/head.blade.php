33339511789bb41df6770d1bc3350883e3a409ee
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-26 22:45:24 +0900

    [add] ビューの共通部分を作成
    headとheaderとfooterを全ページで表示するようにした

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title') | meigorohQ</title>
+    <!-- リセットcss -->
+    <!-- <link rel="stylesheet" href="/freemarket/share/reset.css", type="text/css"> -->
+    <!-- Googleフォント -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!--BootstrapのCSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <!--BootstrapのJS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- 共通のcss -->
+    <!-- <link rel="stylesheet" href="share/style.css", type="text/css"> -->
+</head>
--- resources/views/statements/index.blade.php
+++ resources/views/statements/index.blade.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
+
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Indexページ</title>
-</head>
-<body>
-    <h1>Indexページ</h1>
-    <table>
-        <tr>
-            <th>title</th>
-            <th>who</th>
-            <th>statement</th>
-        </tr>
-        @foreach($items as $item)
-        <tr>
-            <td>{{$item->title}}</td>
-            <td>{{$item->who}}</td>
-            <td>{{$item->statement}}</td>
-        </tr>
-        @endforeach
-    </table>
-</body>
+
+@extends('layouts.master')
+
+@section('title', 'indexページ')
+
+@section('container')
+<h1>Indexページ</h1>
+<table>
+    <tr>
+        <th>title</th>
+        <th>who</th>
+        <th>statement</th>
+    </tr>
+    @foreach($items as $item)
+    <tr>
+        <td>{{$item->title}}</td>
+        <td>{{$item->who}}</td>
+        <td>{{$item->statement}}</td>
+    </tr>
+    @endforeach
+</table>
+@endsection
+
+
+
 </html>
