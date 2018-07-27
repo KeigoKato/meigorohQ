939f826dd9b8312f31cea391f7c054ee959984ab
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-28 01:42:17 +0900

    [add] searchアクションを実装
    検索はヘッダーにある検索欄からおこなう

--- app/Http/Controllers/StatementsController.php
+++ app/Http/Controllers/StatementsController.php
@@ -18,4 +18,19 @@ class StatementsController extends Controller
         $params    = ['items' => $items];
         return view('statements.index', $params);
     }
+
+    /**
+     * Statementモデルのsearchアクション
+     *
+     * @param Request $request
+     * @return statements/searchビュー
+     */
+    public function search(Request $request) {
+        $keyword = $request->keyword;
+        $results = Statement::where('title', 'like', '%'.$keyword.'%')
+        ->orWhere('who', 'like', '%'.$keyword.'%')
+        ->orWhere('statement', 'like', '%'.$keyword.'%')
+        ->get();
+        return view('statements.search', ['results'=>$results, 'keyword'=>$keyword]);
+    }
 }
