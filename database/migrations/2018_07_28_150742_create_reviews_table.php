5e8f276d02ceb0c2ce6d710b6df2666b7c8e6dca
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-29 00:30:40 +0900

    [add] reviewsテーブルを追加
    後で変更を加えていく

---
+++ database/migrations/2018_07_28_150742_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('star');
+            $table->string('comment');
+            // $table->integer('user_id');
+            $table->unsignedInteger('statement_id');
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('statement_id')->references('id')->on('statements');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
