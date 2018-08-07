8de55499314a27303ef37dfbd52def99eaefbdf0
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-08-08 00:10:52 +0900

    [fix] 標準の認証用ビューを使わずに認証を実施
    ログアウト後のリダイレクトに苦戦した

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,16 @@
 <?php
-/**
- * Statementモデルに関するのルーティング
- */
-Route::get('statements/index', 'StatementsController@index')->middleware('sort');
-Route::get('statements/search', 'StatementsController@search')->middleware('sort');
-Route::get('statements/show', 'StatementsController@show');
-Route::get('statements/add', 'StatementsController@add');
-Route::post('statements/add', 'StatementsController@create');
-
-/**
- * Reviewモデルに関するルーティング
- */
-Route::post('/reviews/add', 'ReviewsController@create');
-Route::post('/reviews/update', 'ReviewsController@update');
-Route::post('/reviews/delete', 'ReviewsController@delete');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('statements/index', 'StatementsController@index')->middleware('sort');
+Route::get('statements/search', 'StatementsController@search')->middleware('sort');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('statements/show', 'StatementsController@show');
+    Route::get('statements/add', 'StatementsController@add');
+    Route::post('statements/add', 'StatementsController@create');
+    Route::post('/reviews/add', 'ReviewsController@create');
+    Route::post('/reviews/update', 'ReviewsController@update');
+    Route::post('/reviews/delete', 'ReviewsController@delete');
+});
